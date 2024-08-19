37937b4348d19fa7a3fee3d870fa4fa1e08c242e
tsubasa-alife <tsubasa.robotics@gmail.com>
2024-08-20 08:40:49 +0900

    対戦機能を実装するところまで

--- Ch06/Program.cs
+++ Ch06/Program.cs
@@ -1,16 +1,34 @@
 ﻿using Ch06;
 
 Console.WriteLine("Hello, Ch06 !");
-
-var state = new SimultaneousMazeState(Constants.SEED);
-var battleAI = new BattleAI(Constants.SEED);
-var primitiveMonteCarloAI = new PrimitiveMonteCarloAI(Constants.SEED);
-Console.WriteLine(state.ToString());
-
-while (!state.IsDone())
+double firstPlayerWinRate = 0;
+int gameNumber = 100;
+for (int i = 0; i < gameNumber; i++)
 {
-	state.Advance(primitiveMonteCarloAI.GetPrimitiveMonteCarloAction(state, 0, 500), battleAI.randomAction(state, 1));
+	var state = new SimultaneousMazeState(Constants.SEED);
+	var battleAI = new BattleAI(Constants.SEED);
+	var primitiveMonteCarloAI = new PrimitiveMonteCarloAI(Constants.SEED);
 	Console.WriteLine(state.ToString());
+
+	while (!state.IsDone())
+	{
+		state.Advance(primitiveMonteCarloAI.GetPrimitiveMonteCarloAction(state, 0, 500), battleAI.randomAction(state, 1));
+	}
+
+	double winRatePoint = state.GetFirstPlayerScoreForWinRate();
+	if (winRatePoint >= 0)
+	{
+		Console.WriteLine(state.ToString());
+	}
+
+	firstPlayerWinRate += winRatePoint;
+	
+	Console.WriteLine($"i {i} w {firstPlayerWinRate / (i + 1)}");
 }
+
+firstPlayerWinRate /= (double)gameNumber;
+Console.WriteLine($"Winning rate of MonteCarlo to Random:\t{firstPlayerWinRate}");
+
+
 
 Console.WriteLine("Game End !!");
